bc33f866112458bb49cd9b8fceb6977d0dc0e7f8
_isolate <114854072+dfiudfv@users.noreply.github.com>
2025-03-21 11:05:21 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main(int argc, char **argv)
 {
     printf("Hello world!\n");
+	printf("Hello world! spasiteeeeeeeeeeeee\n");
     return 0;
 	//cod fignia davai delai vse po novoi 
 	// spasiteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
